939499fad73e6d8cfa91ed4b7464c32cba4c0855
Aziz Shoev <slycat1795@gmail.com>
2023-03-07 20:31:28 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int clientBalance = 500;
+        int upAmountBalance = 1590;
+        int bonus = 0;
+
+        if (upAmountBalance > 1000) {
+            bonus = (upAmountBalance / 100 * 1);
+        }
+
+        int totalBalance = clientBalance + upAmountBalance + bonus;
+
+        System.out.println("Сумма пополнения счета: " + upAmountBalance + " рублей");
+        System.out.println("Сумма бонуса: " + bonus + " рублей");
+        System.out.println("Итоговый баланс: " + totalBalance + " рублей");
+    }
+
+}
